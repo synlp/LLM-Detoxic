8216c30f3471e3468b09dc0f8fa5e0e2f5cdf276
futruer <115360588+futruer@users.noreply.github.com>
2025-06-03 10:47:28 +0800

    Update run_generation.sh

--- scripts/run_generation.sh
+++ scripts/run_generation.sh
@@ -1,8 +1,5 @@
-#!/bin/bash
-
-# 参数检查
-if [ $# -ne 5 ]; then
-    echo "Usage: $0 <teacher_model_path> <target_model_path> <matrix_A_path> <device> <output_path>"
+if [ $# -ne 6 ]; then
+    echo "Usage: $0 <teacher_model_path> <target_model_path> <matrix_A_path> <device> <output_path> <data_path>"
     exit 1
 fi
 
@@ -11,10 +8,12 @@ TARGET_MODEL=$2
 MATRIX_A=$3
 DEVICE=$4
 OUTPUT_PATH=$5
+DATA_PATH=$6 
 
 python run_generation.py \
     --teacher_model "$TEACHER_MODEL" \
     --target_model "$TARGET_MODEL" \
     --matrix_a "$MATRIX_A" \
     --device "$DEVICE" \
-    --output_path "$OUTPUT_PATH"
+    --output_path "$OUTPUT_PATH" \
+    --data_path "$DATA_PATH" 
